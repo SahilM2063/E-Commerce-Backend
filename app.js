fc0c7f125489720aae910a385c620c1f49c9e72d
SahilM2063 <makadiasahil9846@gmail.com>
2024-02-24 12:16:56 +0530

    Webhook CLI implemented and All SetUp completed for WebHook Implementation

--- app.js
+++ app.js
@@ -27,6 +27,41 @@ app.use('/api/v1/colors', colorRoutes);
 app.use('/api/v1/reviews', reviewRoutes);
 app.use('/api/v1/orders', orderRoutes);
 
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = process.env.WEBHOOK_ENDPOINT_SECRET;
+
+app.post('/webhook', express.raw({ type: 'application/json' }), (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntentSucceeded = event.data.object;
+            // Then define and call a function to handle the event payment_intent.succeeded
+            break;
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+});
+
+
 // not found handler
 app.use(notFound);
 // global error handler
